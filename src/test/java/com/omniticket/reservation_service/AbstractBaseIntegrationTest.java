b8d68fa8f66825ae62d879f9548d40105fb2f2f3
MixhizoR <oguzselmancetin@hotmail.com>
2026-06-13 17:30:47 +0300

    chore: add impplan.md to .gitignore and reformat test configuration

--- src/test/java/com/omniticket/reservation_service/AbstractBaseIntegrationTest.java
+++ src/test/java/com/omniticket/reservation_service/AbstractBaseIntegrationTest.java
@@ -17,21 +17,51 @@ public abstract class AbstractBaseIntegrationTest {
     @DynamicPropertySource
     static void configureProperties(DynamicPropertyRegistry registry) {
         // PostgreSQL
-        registry.add("spring.datasource.url",
-                () -> "jdbc:postgresql://" + TestContainerSingleton.getPostgres().getHost()
-                        + ":" + TestContainerSingleton.getPostgres().getFirstMappedPort()
-                        + "/" + TestContainerSingleton.getPostgres().getDatabaseName());
-        registry.add("spring.datasource.username", TestContainerSingleton.getPostgres()::getUsername);
-        registry.add("spring.datasource.password", TestContainerSingleton.getPostgres()::getPassword);
+        registry.add(
+            "spring.datasource.url",
+            () ->
+                "jdbc:postgresql://" +
+                TestContainerSingleton.getPostgres().getHost() +
+                ":" +
+                TestContainerSingleton.getPostgres().getFirstMappedPort() +
+                "/" +
+                TestContainerSingleton.getPostgres().getDatabaseName()
+        );
+        registry.add(
+            "spring.datasource.username",
+            TestContainerSingleton.getPostgres()::getUsername
+        );
+        registry.add(
+            "spring.datasource.password",
+            TestContainerSingleton.getPostgres()::getPassword
+        );
 
         // Redis
-        registry.add("spring.data.redis.host", TestContainerSingleton.getRedis()::getHost);
-        registry.add("spring.data.redis.port", TestContainerSingleton.getRedis()::getFirstMappedPort);
+        registry.add(
+            "spring.data.redis.host",
+            TestContainerSingleton.getRedis()::getHost
+        );
+        registry.add(
+            "spring.data.redis.port",
+            TestContainerSingleton.getRedis()::getFirstMappedPort
+        );
 
         // RabbitMQ
-        registry.add("spring.rabbitmq.host", TestContainerSingleton.getRabbitmq()::getHost);
-        registry.add("spring.rabbitmq.port", TestContainerSingleton.getRabbitmq()::getAmqpPort);
-        registry.add("spring.rabbitmq.username", TestContainerSingleton.getRabbitmq()::getAdminUsername);
-        registry.add("spring.rabbitmq.password", TestContainerSingleton.getRabbitmq()::getAdminPassword);
+        registry.add(
+            "spring.rabbitmq.host",
+            TestContainerSingleton.getRabbitmq()::getHost
+        );
+        registry.add(
+            "spring.rabbitmq.port",
+            TestContainerSingleton.getRabbitmq()::getAmqpPort
+        );
+        registry.add(
+            "spring.rabbitmq.username",
+            TestContainerSingleton.getRabbitmq()::getAdminUsername
+        );
+        registry.add(
+            "spring.rabbitmq.password",
+            TestContainerSingleton.getRabbitmq()::getAdminPassword
+        );
     }
 }
